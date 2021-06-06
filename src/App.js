e01da326f9733b632502e65d5f7b7ea301d8bee9
KryptomSilver <abelromeror763@gmail.com>
2021-06-06 00:49:00 -0500

    View 404 Not found

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Login from "./views/Login";
 import "bootswatch/dist/slate/bootstrap.min.css";
 import "./App.css";
 import Home from "./views/Home";
+import NotFound from "./views/NotFound";
 function App() {
     return (
         <Router>
             <Switch>
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/home" component={Home} />
+                <Route  path="*" component={NotFound} />
             </Switch>
         </Router>
     );
---
+++ src/views/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../css/404.css'
+const NotFound = () => {
+    return (
+        <div className="contenedor">
+            <div title="404" className="animated">
+                404
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
